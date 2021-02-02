46fbb4444d6813845f52c9cbed4c784ab9875bb8
Anne, Sriram <sanne31@gatech.edu>
2021-02-02 00:41:57 -0500

    added teacher child class extending from person and implementing the methods and construcros

---
+++ src/com/company/Teacher.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Teacher  extends Person{
+
+    private String subjectName;
+    private double salary;
+
+    public Teacher(String firstName, String lastName, String subjectName, double salary) {
+        super(firstName, lastName);
+        this.subjectName = subjectName;
+        this.salary = salary;
+    }
+
+    public Teacher(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+}
